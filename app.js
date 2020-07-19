ad6c79a6482902a622a06b873b0e14cc4bbe5c0a
DhanushSS <67519006+DhanushSS@users.noreply.github.com>
2020-07-19 12:37:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+function copy() {
+    alert("SORRY YOU CAN'T DO IT");
+}
+
+function Summer() {
+    document.body.style.backgroundImage = 'url("https://res.cloudinary.com/dkd5jyhic/image/upload/v1594968327/Summer_tf9yzu.png")';
+    document.body.style.backgroundRepeat = "no repeat";
+    document.body.style.backgroundSize = "100%";
+}
+
+function Winter() {
+    document.body.style.backgroundImage = 'url("https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png")';
+    document.body.style.backgroundRepeat = "no repeat";
+    document.body.style.backgroundSize = "100%";
+}
+
+function Autumn() {
+    document.body.style.backgroundImage = 'url("https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png")';
+    document.body.style.backgroundRepeat = "no repeat";
+    document.body.style.backgroundSize = "100%";
+}
+
+function Spring() {
+    document.body.style.backgroundImage = 'url("https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png")';
+    document.body.style.backgroundRepeat = "no repeat";
+    document.body.style.backgroundSize = "100%";
+}
